c777fd3eeeddb74edb84eeb6b6f6bea50463089d
gaorui <gaorui@lianjia.com>
2017-07-05 18:06:49 +0800

    0705

---
+++ src/data/article.js
@@ -0,0 +1,163 @@
+
+	export default [
+	{
+		"title":"Alert62","path":"/Alert62","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/Alert62.md')
+
+	},
+	{
+		"title":"index","path":"/index","createTime":"2017年07月28日22:22:22","component":()=>System.import('article/index.md')
+
+	},
+	{
+		"title":"Button","path":"/Button","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/Button.md')
+
+	},
+	{
+		"title":"Checkbox","path":"/Checkbox","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/Checkbox.md')
+
+	},
+	{
+		"title":"Confirm","path":"/Confirm","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/Confirm.md')
+
+	},
+	{
+		"title":"Form","path":"/Form","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/Form.md')
+
+	},
+	{
+		"title":"Icon","path":"/Icon","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/Icon.md')
+
+	},
+	{
+		"title":"ImgView","path":"/ImgView","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/ImgView.md')
+
+	},
+	{
+		"title":"Input","path":"/Input","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/Input.md')
+
+	},
+	{
+		"title":"Layout","path":"/Layout","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/Layout.md')
+
+	},
+	{
+		"title":"LeftMenu","path":"/LeftMenu","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/LeftMenu.md')
+
+	},
+	{
+		"title":"Modal","path":"/Modal","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/Modal.md')
+
+	},
+	{
+		"title":"Pagination","path":"/Pagination","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/Pagination.md')
+
+	},
+	{
+		"title":"Photo","path":"/Photo","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/Photo.md')
+
+	},
+	{
+		"title":"Select","path":"/Select","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/Select.md')
+
+	},
+	{
+		"title":"Tab","path":"/Tab","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/Tab.md')
+
+	},
+	{
+		"title":"Table","path":"/Table","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/Table.md')
+
+	},
+	{
+		"title":"Tree","path":"/Tree","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/Tree.md')
+
+	},
+	{
+		"title":"Upload","path":"/Upload","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/Upload.md')
+
+	},
+	{
+		"title":"Alert","path":"/article/Alert","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/article/Alert.md')
+
+	},
+	{
+		"title":"Breadcrumb","path":"/Breadcrumb","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/Breadcrumb.md')
+
+	},
+	{
+		"title":"Button","path":"/article/Button","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/article/Button.md')
+
+	},
+	{
+		"title":"Checkbox","path":"/article/Checkbox","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/article/Checkbox.md')
+
+	},
+	{
+		"title":"Confirm","path":"/article/Confirm","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/article/Confirm.md')
+
+	},
+	{
+		"title":"Form","path":"/article/Form","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/article/Form.md')
+
+	},
+	{
+		"title":"Icon","path":"/article/Icon","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/article/Icon.md')
+
+	},
+	{
+		"title":"ImgView","path":"/article/ImgView","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/article/ImgView.md')
+
+	},
+	{
+		"title":"Input","path":"/article/Input","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/article/Input.md')
+
+	},
+	{
+		"title":"Layout","path":"/article/Layout","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/article/Layout.md')
+
+	},
+	{
+		"title":"LeftMenu","path":"/article/LeftMenu","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/article/LeftMenu.md')
+
+	},
+	{
+		"title":"Modal","path":"/article/Modal","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/article/Modal.md')
+
+	},
+	{
+		"title":"Pagination","path":"/article/Pagination","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/article/Pagination.md')
+
+	},
+	{
+		"title":"Photo","path":"/article/Photo","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/article/Photo.md')
+
+	},
+	{
+		"title":"Select","path":"/article/Select","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/article/Select.md')
+
+	},
+	{
+		"title":"Tab","path":"/article/Tab","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/article/Tab.md')
+
+	},
+	{
+		"title":"Table","path":"/article/Table","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/article/Table.md')
+
+	},
+	{
+		"title":"Tree","path":"/article/Tree","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/article/Tree.md')
+
+	},
+	{
+		"title":"Upload","path":"/article/Upload","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/article/Upload.md')
+
+	},
+	{
+		"title":"index","path":"/article/index","createTime":"2017年07月28日22:22:22","component":()=>System.import('article/article/index.md')
+
+	},
+	{
+		"title":"Breadcrumb","path":"/article/Breadcrumb","createTime":"2017-7-5 16:20:41","component":()=>System.import('article/article/Breadcrumb.md')
+
+	}]
+	
