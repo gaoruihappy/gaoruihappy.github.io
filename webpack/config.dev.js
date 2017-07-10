54146afdd3bcc75752e8ce7a6d18b16b2429b1a1
gaorui <gaorui@lianjia.com>
2017-07-10 16:29:15 +0800

    0710_1ok

--- webpack/config.dev.js
+++ webpack/config.dev.js
@@ -1,11 +1,22 @@
 var common = require('./config.common')
 const fs = require('fs')
 const path = require('path')
+const SyncMDDataPlugin = require('./SyncMDDataPlugin')
+
 module.exports = function (webpackConfig, redSkull, webpackPlugins) {
 
   webpackConfig = common(webpackConfig, redSkull, webpackPlugins)
 
   webpackConfig.devtool = 'source-map'
+
+  fs.readdirSync(redSkull.src).map(dir =>{
+  	const fp = path.join(redSkull.src,dir)
+  	if(fs.statSync(fp).isDirectory()){
+  		webpackConfig.resolve.alias[dir] = path.join(redSkull.src,dir)
+  	}
+  })
+
+  webpackConfig.plugins.push(new SyncMDDataPlugin())
 
   return webpackConfig
 }
